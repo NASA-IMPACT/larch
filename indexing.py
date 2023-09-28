25e2bbb0edf00a6b72a0a5ced905c5a01247735a
NISH1001 <nishanpantha@gmail.com>
2023-09-28 13:11:21 -0500

    Add initial code structure for metadata extractor
    See `larch.metadata`

---
+++ indexing.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+import json
+import pickle
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Optional, Type
+
+from langchain.chat_models import ChatOpenAI
+from loguru import logger
+from paperqa import Docs
+from pydantic import BaseModel
+
+from .metadata import AbstractMetadataExtractor
+
+
+class DocumentIndexer(ABC):
+    """
+    Abstract Base Class for setting up document indexer type
+    """
+
+    def __init__(self, docs: Optional[List[str]] = None, debug: bool = False) -> None:
+        self.debug = debug
+        self.docs = docs or []
+        self.doc_store = None
+
+    @abstractmethod
+    def index_documents(self, docs: List[str], **kwargs) -> Any:
+        raise NotImplementedError()
+
+    def __call__(self, *args, **kwargs) -> Any:
+        return self.index_documents(*args, **kwargs)
+
+    def save_index(self, path: str):
+        raise NotImplementedError()
+
+    def load_index(self, path: str):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def query(self, query: str, **kwargs) -> str:
+        raise NotImplementedError()
+
+
+class PaperQADocumentIndexer(DocumentIndexer):
+    """
+    Uses paperqa indexing pipeline to index documents provided that we have a list of paths.
+    """
+
+    def __init__(
+        self,
+        llm: Optional[ChatOpenAI] = None,
+        docs: Optional[List[str]] = None,
+        doc_store: Optional[Docs] = None,
+        debug: bool = False,
+    ) -> None:
+        super().__init__(docs=docs, debug=debug)
+        llm = llm or ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0)
+        self.doc_store = doc_store or Docs(llm=llm)
+
+    def index_documents(self, paths: List[str], **kwargs) -> Any:
+        for path in paths:
+            if path in self.docs:
+                continue
+            if self.debug:
+                print(f"Creating index for src={path}")
+            self.doc_store.add(path)
+        if self.debug:
+            print(f"Total of {len(self.doc_store.texts)} pages indexed.")
+
+        self.docs.extend(paths)
+        return self
+
+    def save_index(self, path: str):
+        if not self.doc_store:
+            return
+        dump_val = dict(docs=self.docs, doc_store=self.doc_store)
+        logger.info(f"Saving document index to {path}")
+        with open(path, "wb") as f:
+            pickle.dump(dump_val, f)
+        return self
+
+    def load_index(self, path: str):
+        with open(path, "rb") as f:
+            dump_val = pickle.load(f)
+            self.docs, self.doc_store = dump_val["docs"], dump_val["doc_store"]
+        return self
+
+    def query(self, query: str, **kwargs) -> str:
+        return self.doc_store.query(query).answer
+
+
+class DocumentMetadataIndexer(DocumentIndexer):
+    """
+    This uses metadata extractor to index the metadata from documents.
+    For now, we store metadata into a dictionary as an index.
+    """
+
+    def __init__(
+        self,
+        schema: Type[BaseModel],
+        *,
+        metadata_extractor: Type[AbstractMetadataExtractor] = None,
+        debug: bool = False,
+    ) -> None:
+        super().__init__(debug=debug)
+        self.metadata_extractor = (
+            metadata_extractor
+            or InstructorBasedOpenAIMetadataExtractor(
+                model="gpt-3.5-turbo-0613",
+                schema=schema,
+                debug=debug,
+            )
+        )
+        self.metadata_store = {}
+
+    @property
+    def schema(self) -> Type[BaseModel]:
+        return self.metadata_extractor.schema
+
+    def index_documents(self, paths: List[str]) -> Dict[str, BaseModel]:
+        mstore = {}
+        for p in paths:
+            if p in self.metadata_store:
+                mstore[p] = self.metadata_store[p]
+                continue
+
+            text = read_doc(p, Doc(citation=p, dockey=p, docname=p))
+            text = map(lambda x: x.text, text)
+            text = "\n".join(text)
+
+            hsh = str(hash(text))
+            if hsh in self.metadata_store:
+                mstore[hsh] = self.metadata_store[hsh]
+                continue
+
+            if self.debug:
+                logger.debug(f"Extracting metadata from {p}")
+
+            mstore[p] = self.metadata_extractor(text)
+
+        self.metadata_store.update(mstore)
+        return self
+
+    def save_index(self, path: str) -> None:
+        dump_val = {}
+        for k, v in self.metadata_store.items():
+            dump_val[k] = v.dict()
+        with open(path, "w") as f:
+            json.dump(dump_val, f)
+        return self
+
+    def load_index(self, path: str) -> None:
+        with open(path, "r") as f:
+            dump_val = json.load(f)
+            for k, v in dump_val.items():
+                self.metadata_store[k] = self.schema.model_construct(**v)
+        return self
+
+    @property
+    def doc_store(self) -> dict:
+        return self.metadata_store
+
+    @doc_store.setter
+    def doc_store(self, value):
+        self.metadata_store = value
+
+    def query(self, *args, **kwargs):
+        raise NotImplementedError()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ metadata.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+import contextlib
+import json
+import re
+from abc import ABC, abstractmethod
+from typing import Callable, List, Optional, Type
+
+import instructor
+import openai
+from langchain.chains import create_extraction_chain
+from langchain.chat_models.base import BaseLanguageModel
+from loguru import logger
+from pydantic import BaseModel, ValidationError
+
+
+class AbstractMetadataExtractor(ABC):
+    """
+    Each MetadataExtractor can be used like a functor through __call__.
+    """
+
+    def __init__(
+        self,
+        preprocess_func: Optional[Callable] = None,
+        debug: bool = False,
+    ) -> None:
+        self.debug = debug
+        self.preprocess_func = preprocess_func or self._preprocess_text
+
+    def extract_metadata(self, text: str) -> Optional[Type[BaseModel]]:
+        text = self._preprocess_text(text)
+        return self._extract(text) if text else None
+
+    @abstractmethod
+    def _extract(self, text: str):
+        raise NotImplementedError()
+
+    @staticmethod
+    def _preprocess_text(text: str):
+        text = re.sub(r"\s+", " ", text)
+        text = text.strip()
+        return text
+
+    def __call__(self, *args, **kwargs):
+        return self.extract_metadata(*args, **kwargs)
+
+
+class SimpleOpenAIMetadataExtractor(AbstractMetadataExtractor):
+    """
+    Barebone extractor that just hits OpenAI API based on instructor patching.
+    No function calling.
+    TODO:
+        Bugfix nasty side-effects because of instructor patch.
+        Calling this 2nd time after we called `InstructorBasedOpenAIMetadataExtractor`
+        throws validation error.
+    """
+
+    _SYSTEM_PROMPT = (
+        "Extract metadata and entities details accurately from my requests."
+    )
+
+    def __init__(
+        self,
+        schema: Type[BaseModel],
+        model: str = "gpt-3.5-turbo-0613",
+        system_prompt: str = _SYSTEM_PROMPT,
+        debug: bool = False,
+    ) -> None:
+        super().__init__(debug=debug)
+        self.model = model
+        self.schema = schema
+        self._system_prompt = system_prompt
+
+    def _get_messages(self, text: str) -> List[dict]:
+        messages = []
+
+        if self._system_prompt:
+            messages.append({"role": "system", "content": self._system_prompt})
+
+        messages.append({"role": "user", "content": text})
+
+        return messages
+
+    def _extract(self, text: str):
+        instructor.patch()
+        metadata = openai.ChatCompletion.create(
+            model=self.model,
+            temperature=0,
+            response_model=self.schema,
+            messages=self._get_messages(text),
+        )
+        instructor.unpatch()
+        return metadata
+
+
+class InstructorBasedOpenAIMetadataExtractor(SimpleOpenAIMetadataExtractor):
+    """
+    This uses `instructor` to hit the OpenAI function calling api.
+    Note: The schema for metadata should be of `Type[OpenAISchema]`.
+    """
+
+    def _extract(self, text: str):
+        instructor.patch()
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            temperature=0,
+            functions=[self.schema.openai_schema],
+            function_call={"name": self.schema.openai_schema["name"]},
+            messages=self._get_messages(text),
+        )
+        if self.debug:
+            logger.debug(response)
+
+        result = self.schema.model_construct()
+
+        try:
+            result = self.schema.from_response(response)
+        except ValidationError as e:
+            logger.warning("Bypassing validation error!")
+            message = response["choices"][0]["message"]
+            result = (
+                self.schema.model_construct(
+                    **json.loads(message["function_call"]["arguments"]),
+                )
+                if "function_call" in message
+                else result
+            )
+        instructor.unpatch()
+        return result
+
+
+class LangchainBasedMetadataExtractor(AbstractMetadataExtractor):
+    """
+    This uses barebone langchain's extraction chain.
+    """
+
+    def __init__(
+        self,
+        schema: Type[BaseModel],
+        llm: Optional[Type[BaseLanguageModel]] = None,
+        debug: bool = False,
+    ):
+        super().__init__(debug=debug)
+        self.llm = llm or ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+        self.schema = schema
+        self.chain = self._create_chain(schema=schema, llm=llm)
+
+    def _create_chain(self, schema, llm):
+        if not isinstance(schema, dict):
+            schema = schema.model_json_schema()
+        return create_extraction_chain(schema=schema, llm=llm)
+
+    def _extract(self, text: str):
+        response = self.chain.run(text)
+        if self.debug:
+            logger.debug(response)
+        return self.schema.model_construct(**response[0]) if response else response
