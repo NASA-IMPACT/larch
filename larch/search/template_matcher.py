88bd9fead5c915281fd3183e061ce9356073ca53
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-09 12:44:48 -0600

    [Feat]: #3 Add method to fill the SQL template with fields from query.

--- larch/search/template_matcher.py
+++ larch/search/template_matcher.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+import re
 from typing import Any, List, Optional
 from langchain.base_language import BaseLanguageModel
 from langchain.chat_models import ChatOpenAI
@@ -53,6 +54,111 @@ class FuzzySQLTemplateMatcher(SQLTemplateMatcher):
         super().__init__(templates=templates,
                          similarity_threshold=similarity_threshold,
                          debug = debug)
+        
+    def get_columns(self, query: str) -> List[str]:
+        """
+        Extracts columns from the user query.
+
+        Args:
+            query (str): User query string.
+
+        Returns:
+            List[str]: List of column names.
+        """
+        print("\n\n\n\n\n hellow world \n\n\n\n\n")
+        columns = []
+        # Use regular expression to identify keywords in the query
+        match = re.search(
+            r"(what|which|how many) (\w+)(?: and (\w+))?", query, re.IGNORECASE)
+        if match:
+            # Extract captured words from the regex match
+            captured_words = match.groups()[1:]
+            # Convert captured words to lowercase
+            captured_words = [word.lower() for word in captured_words if word]
+            print(f"Captured culumns: {captured_words}")
+            for captured_word in captured_words:
+                # Map captured words to corresponding SQL columns
+                if captured_word == "agencies" or captured_word == "agency":
+                    columns.append("metadata.organization_name")
+                elif captured_word == "needs" or captured_word == "need":
+                    columns.append("need.need_nature")
+                elif captured_word == "satellite" or captured_word == "satellites":
+                    columns.append("solution.platform")
+                elif captured_word == "product" or captured_word == "products":
+                    columns.append("solution.data_product")
+                elif captured_word == "solution" or captured_word == "solutions":
+                    columns.append("solution.id")
+        print(f"returning columns: {columns}")
+        return columns
+
+    def get_comparison_key(self, comparison_value: str) -> str:
+        """
+        Determines the comparison key based on the comparison value.
+
+        Args:
+            comparison_value (str): Comparison value extracted from the user query.
+
+        Returns:
+            str: Comparison key.
+        """
+        # Map comparison values to corresponding SQL comparison keys
+        if "satellite" in comparison_value or "mission" in comparison_value:
+            return "solution.platform"
+        elif "product" in comparison_value:
+            return "solution.data_product"
+        elif "observable" in comparison_value or "observe" in comparison_value:
+            return "solution.observable"
+        if "phenomenon" in comparison_value:
+            return "need.phenomenon"
+
+    def get_comparing_value(self, query: str) -> Optional[str]:
+        """
+        Extracts the comparing value from the user query.
+
+        Args:
+            query (str): User query string.
+
+        Returns:
+            Optional[str]: Comparing value or None if not found.
+        """
+        # Use regular expression to extract comparing value from the query
+        match = re.search(
+            r"(?:using|to use|use to|observe|are used|to observe|relate to|related to| to use| recommended| was| did)(.+?)\?", query, re.IGNORECASE)
+        if match:
+            return match.group(1).strip()
+        else:
+            return None
+        
+    def fill_template(self, template: SQLTemplate, query: str) -> Optional[SQLTemplate]:
+        """
+        Fill the template with the given query.
+
+        Args:
+            template: The template to fill.
+            query: The query to fill the template with.
+        Returns:
+            The filled template.
+        """
+        # Extract columns, comparing value, and comparison key from the user query
+        columns = self.get_columns(query)
+        comparing_value = self.get_comparing_value(query)
+        comparison_key = self.get_comparison_key(comparing_value)
+        # Check if both columns and comparing value are available
+        if columns and comparing_value:
+            # Substitute values into the SQL template
+            args = {
+                "columns": ", ".join(columns),
+                "comparison_key": comparison_key,
+                "comparing_value": comparing_value,
+                "similarity_threshold": self.similarity_threshold
+            }
+            # Substitute the values into a copy of the template
+            template = template.model_copy()
+            template.sql_template = template.sql_template.format(**args)
+            return template
+        else:
+            return None
+
 
     def match(self, query: str, top_k=1, **kwargs) -> List[str]:
         query = query.lower()
@@ -66,6 +172,12 @@ class FuzzySQLTemplateMatcher(SQLTemplateMatcher):
         best_matches = [self.templates[index] 
                         for index, score in match_scores
                           if score >= self.similarity_threshold]
+        # Fill the template with the query
+        best_matches = [self.fill_template(template, query)
+                        for template in best_matches]
+        # Remove None values from the list
+        best_matches = [template for template in best_matches if template]
+        
         return best_matches[:top_k]
         
 
--- tests/test_template_matcher.py
+++ tests/test_template_matcher.py
@@ -6,24 +6,20 @@ class TestFuzzySQLTemplateMatcher:
     def templates(self):
         return [
             SQLTemplate(query_pattern="which what solutions agencies interested relate recommended",
-                        sql_template="""SELECT DISTINCT {columns}
-                        FROM metadata
-                        JOIN solution ON metadata.id = solution.need_id
-                        WHERE {comparison_key} ILIKE '%{comparing_value}%' OR similarity({comparison_key}, '{comparing_value}') > {similarity_threshold};"""
+                        sql_template="""SELECT DISTINCT {columns} FROM metadata JOIN solution ON metadata.id = solution.need_id WHERE {comparison_key} ILIKE '%{comparing_value}%' OR similarity({comparison_key}, '{comparing_value}') > {similarity_threshold};"""
             ),
             SQLTemplate(query_pattern="How many solutions agencies interested relate recommended",
-                        sql_template="""SELECT COUNT(DISTINCT {columns})
-                        FROM metadata
-                        JOIN solution ON metadata.id = solution.need_id
-                        WHERE {comparison_key} ILIKE '%{comparing_value}%' OR similarity({comparison_key}, '{comparing_value}') >= {similarity_threshold};"""
+                        sql_template="""SELECT COUNT(DISTINCT {columns}) FROM metadata JOIN solution ON metadata.id = solution.need_id WHERE {comparison_key} ILIKE '%{comparing_value}%' OR similarity({comparison_key}, '{comparing_value}') >= {similarity_threshold};"""
             ),
         ]
 
     def test_match(self, templates):
         matcher = FuzzySQLTemplateMatcher(templates=templates, similarity_threshold=0.4)
         query1 = "Which agencies are interested in using the ICESat-2 satellite?"
+        sql1 = """SELECT DISTINCT metadata.organization_name FROM metadata JOIN solution ON metadata.id = solution.need_id WHERE solution.platform ILIKE '%the icesat-2 satellite%' OR similarity(solution.platform, 'the icesat-2 satellite') > 0.4;"""
         query2 = "How many solutions recommended the ICESat-2 satellite?"
+        sql2 = """SELECT COUNT(DISTINCT solution.id) FROM metadata JOIN solution ON metadata.id = solution.need_id WHERE solution.platform ILIKE '%the icesat-2 satellite%' OR similarity(solution.platform, 'the icesat-2 satellite') >= 0.4;"""
         result1 = matcher.match(query1)
         result2 = matcher.match(query2)
-        assert result1 == [templates[0].sql_template]
-        assert result2 == [templates[1].sql_template]
+        assert result1[0].sql_template == sql1
+        assert result2[0].sql_template == sql2
