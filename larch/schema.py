f48b4dad827e78066393d9ff8667acafcdc8dc97
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-10 12:55:27 -0600

    [Feat]: #3 Add example subclass in sql template class and refactor.

--- larch/schema.py
+++ larch/schema.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-from typing import List, Optional, Dict, Any
+from typing import Any, Dict, List, Optional
 
 from instructor import OpenAISchema
 from pydantic import BaseModel, Field, create_model
@@ -83,15 +83,30 @@ class Metadata(BaseModel):
 
 class SQLTemplate(BaseModel):
     """
-        SQLTemplate represents a SQL template for a given query pattern.
+    SQLTemplate represents a SQL template for a given query pattern.
 
-        E.g.:
-        query_pattern: "When did <mission_name> launch?"
-        sql_template: "SELECT date_operational FROM  <table_name> WHERE mission_name ILIKE '%<mission_name>%';"
-
+    E.g.:
+    query_pattern: "When did <mission_name> launch?"
+    sql_template: "SELECT date_operational FROM  <table_name> WHERE mission_name ILIKE '%<mission_name>%';"
     """
+
+    class Example(BaseModel):
+        """
+        Example represents an example related to the SQL template.
+
+        Attributes:
+        - query (str): The query example.
+        - sql (str): The corresponding SQL example.
+        - result (Any): The expected result of the example.
+        """
+
+        query: str
+        sql: str
+        result: Any
+
     query_pattern: str
-    sql_template: str
+    sql_pattern: str
+    examples: Optional[List[Example]] = None
     intent: Optional[str] = None
     description: Optional[str] = None
     extras: Optional[Dict[str, Any]] = None
